63a5d36a0651bf51673161a86f9bd52a86dad7e2
Vladimir Berezovsky <vladimirberezovsky@Vladimirs-MacBook-Pro-2.local>
2020-10-12 14:28:13 +0300

    Updates

---
+++ precache-manifest.bd3c8632f1e2da07118a83944b984d86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "926b84a7447f1aeedd1b1483076e8643",
+    "url": "/millionaire-game/index.html"
+  },
+  {
+    "revision": "1159e48d184c34ff73e2",
+    "url": "/millionaire-game/static/css/main.8bfe1deb.chunk.css"
+  },
+  {
+    "revision": "1b07c41f90af689b125a",
+    "url": "/millionaire-game/static/js/2.715fbfdc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/millionaire-game/static/js/2.715fbfdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1159e48d184c34ff73e2",
+    "url": "/millionaire-game/static/js/main.ee7ecdd9.chunk.js"
+  },
+  {
+    "revision": "5c2846d7d876c13c62ed",
+    "url": "/millionaire-game/static/js/runtime-main.23e937b9.js"
+  },
+  {
+    "revision": "276b2ba6b79973c3215716dd829c1be1",
+    "url": "/millionaire-game/static/media/hand.276b2ba6.svg"
+  }
+]);
